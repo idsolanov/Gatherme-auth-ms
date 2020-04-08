3db1ce2e8b098897983afcecfac9c495f1978f58
idsolanov <idsolanov@unal.edu.co>
2020-04-07 23:34:04 -0500

    last update to config.js

--- source/config.js
+++ source/config.js
@@ -1,5 +1,5 @@
 module.exports={
     port : process.env.PORT || 3001,
-    db: process.env.MONGODB|| 'mongodb://localhost:27017/auth_ms',
+    db: process.env.MONGODB|| 'mongodb://gatherme-authentication-db:27017/auth_ms',
     SECRET_TOKEN: 'comoseriacomoesbuenaelgetho'
 }
