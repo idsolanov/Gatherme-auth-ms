8a9106c0aee0335a32750aaefafdeac7e381e924
Ivan Dario Solano Velasquez <idsolanov@unal.edu.co>
2020-06-15 12:01:13 -0500

    rancher3

--- source/config.js
+++ source/config.js
@@ -2,7 +2,7 @@ module.exports={
     port : process.env.PORT || 3001,
     dbhost: 'gatherme-authentication-db',
     localhost: 'localhost',
-    db: process.env.MONGODB|| 'http://192.168.99.114:32681/auth_ms',
+    db: process.env.MONGODB|| 'mongodb://192.168.99.114:32681/auth_ms',
     SECRET_TOKEN: '104179G104183A104207T104231H104233E104239R104243M104281E104287AUTH104297',
     ldap_port:31329,//389,
     ldap_url:'ldap://192.168.99.114',
